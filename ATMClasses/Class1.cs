05831592014cf26795285a5b2625da37cd2a7d35
@Sug7 <sug-2508@hotmail.com>
2019-03-13 10:59:07 +0100

    Evenhandler for decoding

---
+++ ATMClasses/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+namespace ATMClasses
+{
+
+    public class Decoding
+    {
+        private ITransponderReceiver receiver;
+
+        Decoding(ITransponderReceiver myReceiver)
+        {
+            receiver = myReceiver;
+
+            receiver.TransponderDataReady += DataHandler;
+        }
+
+        public void DataHandler(object o, RawTransponderDataEventArgs eventArgs)
+        {
+            List<string> recList = eventArgs.TransponderData;
+        }
+    }
+}
