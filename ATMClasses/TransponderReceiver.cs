86f163c81ef40785e616c76ec9ebfd9504f84b7b
Omaar9 <farhaan9@hotmail.com>
2019-03-19 13:24:36 +0100

    Classes updatet

--- ATMClasses/TransponderReceiver.cs
+++ ATMClasses/TransponderReceiver.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ATMClasses
 {
-    class Tracks
+    class TransponderReceiver
     {
     }
 }
--- AirTrafficGruppe5/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AirTrafficGruppe5
-{
-    public class Class1
-    {
-    }
-}
