e7bc28699faccaa8b00f36b36ac1d9f580bb5100
Ellen Huld Thordardottir <au582001@uni.au.dk>
2019-03-19 21:24:26 +0100

    Decoding class plus Program to test DC

--- ATMClasses/Decoding.cs
+++ ATMClasses/Decoding.cs
@@ -9,19 +9,32 @@ namespace ATMClasses
 
     public class Decoding
     {
+        public class TransponderReceiver : EventArgs
+        {
+            public string Signal { get; set; }
+        }
+
         private ITransponderReceiver receiver;
 
-        Decoding(ITransponderReceiver myReceiver)
+        public Decoding(ITransponderReceiver myReceiver)
         {
             receiver = myReceiver;
 
-            receiver.TransponderDataReady += DataHandler;
+            receiver.TransponderDataReady += DataHandler; //datahandler will be added to an internal list that the event keeps track of (when the owning class fires that event, all the delegates in the list will be called)
+
         }
+
 
         public void DataHandler(object o, RawTransponderDataEventArgs eventArgs)
         {
             List<string> recList = eventArgs.TransponderData;
-            //comment
+           
+            foreach (var er in recList)
+            {
+                Console.WriteLine(er);
+            }
+
+
         }
     }
 }
---
+++ AirTrafficGruppe5Application/Program.cs
@@ -0,0 +1,24 @@
+﻿using ATMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace AirTrafficGruppe5Application
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                var tr = TransponderReceiverFactory.CreateTransponderDataReceiver();
+                var decoding = new Decoding(tr);
+
+                while (true) Thread.Sleep(1000);
+            }
+        
+    }
+}
