c5006a5622ed4e3f557b293dd6b79ea0829cddb6
@Sug7 <sug-2508@hotmail.com>
2019-03-23 13:31:06 +0100

    Writer code and File

--- ATMClasses/File.cs
+++ ATMClasses/File.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ATMClasses
 {
     public class File
     {
+        private object fileWriter;
+
+        public void Update(ConditionDetecter conditionDetecter)
+        {
+            StreamWriter _fileWriter = new StreamWriter(@"C: \Users\Brug\Desktop\AirTrafficGruppe5\ATMClasses\bin\Debug\logfile.txt");
+            
+            _fileWriter.WriteLine(conditionDetecter);
+
+            _fileWriter.Flush();
+        }
+
     }
 }
--- ATMClasses/Writer.cs
+++ ATMClasses/Writer.cs
@@ -1,13 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ATMClasses
 {
-    public class Writer
+    public class Writer : IWriter
     {
 
+        private List<ConditionDetecter> conditionTracks;
+        public event EventHandler<SplitEvent> SplitCreation;
+        public Dictionary<String, ITrack> tracks { get; set; }
+
+        public Writer(IFilter trackfilter, List<ConditionDetecter> conditionTracks)
+        {
+            trackfilter.TrackEdited += onTrackEdited;
+            conditionTracks.SplitCreated += onSplitCreated;
+        }
+
+        public void onTrackEdited(object source, TrackEvent trackEvent)
+        {
+            tracks = trackEvent.tracks;
+            conditionTracks = new List<ConditionDetecter>();
+
+            List<ITrack> flight = new List<ITrack>();
+
+            foreach (var track in trackEvent.tracks)
+            {
+                flight.Add(track.Value);
+            }
+
+            for (int i = 0; i < flight.Count; i++)
+            {
+                for (int j = i + 1; j < flight.Count; j++)
+                {
+                    if (Distance(flight[i], flight[j]) < 5000)
+                    {
+                        if (Math.Abs(flight[i].Altitude_ - flight[j].Altitude_) < 300)
+                        {
+                            List<ITrack> condition = new List<ITrack>();
+                            condition.Add(flight[i]);
+                            condition.Add(flight[j]);
+                            conditionTracks.Add(new ConditionDetecter(condition));
+                        }
+                    }
+                }
+            }
+
+            if (conditionTracks.Count > 0)
+            {
+                onSplitCreated(conditionTracks);
+            }
+        }
+        
+        public double Distance(ITrack track1, ITrack track2)
+        {
+            double hyp1 = Math.Sqrt(Math.Pow((track1.X_coordinates - track2.X_coordinates), 2) + Math.Pow((track1.Y_coordinates - track2.Y_coordinates), 2));
+            return Math.Abs(hyp1);
+        }
+        protected virtual void onSplitCreated(List<ConditionDetecter> condtiontracks)
+        {
+            SplitCreated?.Invoke(this, new SplitEvent() {ConditionDetecters = condtiontracks});
+        }
+        public class SplitEvent : EventArgs
+        {
+            public List<ConditionDetecter> ConditionDetecters { get; set; }
+        }
+        public void onSplitCreated(object source, SplitEvent condition)
+        {
+            Console.Clear();
+
+            String[] l = new string[condition.ConditionDetecters.Count];
+
+            for (int i = 0; i < condition.ConditionDetecters.Count; i++)
+            {
+                l[i] = "Flight are in DANGER";
+
+                for (int j = 0; j < condition.ConditionDetecters[i]._detectingFligts.Count; j++)
+                {
+                    l[i] += condition.ConditionDetecters[i]._detectingFligts[j].Tag_ + ", ";
+                }
+
+                l[i] += condition.ConditionDetecters[i]._detectTime + ":" +
+                        condition.ConditionDetecters[i]._detectTime.Millisecond;
+            }
+
+            for (int i = 0; i < l.Length; i++)
+            {
+                Console.WriteLine(l[i]);
+            }
+        
+        }
+ 
     }
 }
